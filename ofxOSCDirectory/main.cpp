3e0b1460348e68eac8183402a4553ed3759eff22
nami634 <cigarbox.nami@gmail.com>
2017-11-03 17:28:37 +0900

    Initial Commit

---
+++ ofxOSCDirectory/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ofxOSCDirectory
+//
+//  Created by nami on 2017/11/03.
+//  Copyright © 2017年 Kousaku Namikawa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
