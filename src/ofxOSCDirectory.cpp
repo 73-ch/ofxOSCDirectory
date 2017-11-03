1a1a8e00f542379c3e5462793facf04dac5a9e69
nami634 <cigarbox.nami@gmail.com>
2017-11-03 17:45:14 +0900

    initial commit

---
+++ src/ofxOSCDirectory.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  ofxOSCDirectory
+//
+//  Created by nami on 2017/11/03.
+//  Copyright © 2017年 Kousaku Namikawa. All rights reserved.
+//
+
+#include "ofxOSCDirectory.h"
---
+++ src/ofxOSCDirectory.h
@@ -0,0 +1,13 @@
+//
+//  ofxOSCDirectory.h
+//  ofxOSCDirectory
+//
+//  Created by nami on 2017/11/03.
+//  Copyright © 2017年 Kousaku Namikawa. All rights reserved.
+//
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+
